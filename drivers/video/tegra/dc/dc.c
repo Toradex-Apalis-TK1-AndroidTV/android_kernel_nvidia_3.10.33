69824da562bc4694376705c00bd710b62979199c
Jong Kim <jongk@nvidia.com>
2014-06-05 10:56:58 -0700

    video: tegra: dc: get default mode from EDID
    Override hard-coded default HDMI mode with the preferred mode
    obtained from EDID read. Since the preferred mode is obtained
    dynamically from EDID, the default hard-coded HDMI mode is set
    to 640x480 @60Hz, which is universally supported.
    
    bug 1495496
    bug 200009711
    
    Change-Id: Ib21109ebd5fcff4e0a825d8b20df9357cf08619e
    Signed-off-by: Jong Kim <jongk@nvidia.com>
    Reviewed-on: http://git-master/r/393564
    (cherry picked from commit 151ac024bc3d27bee424aa33725988e8bf18375d)
    Reviewed-on: http://git-master/r/419656
    Reviewed-by: Mandar Padmawar <mpadmawar@nvidia.com>
    Tested-by: Mandar Padmawar <mpadmawar@nvidia.com>
    
    Conflicts:
    	drivers/video/tegra/dc/dc.c

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -3160,6 +3160,33 @@ static int tegra_dc_probe(struct platform_device *ndev)
 	}
 	dc->mode_dirty = false; /* ignore changes tegra_dc_set_out has done */
 
+#ifdef CONFIG_FRAMEBUFFER_CONSOLE
+	if (dc->out && dc->out->n_modes &&
+	    (dc->out->type == TEGRA_DC_OUT_HDMI)) {
+		struct fb_monspecs specs;
+		struct tegra_dc_hdmi_data *hdmi = tegra_dc_get_outdata(dc);
+		if (!tegra_edid_get_monspecs(hdmi->edid, &specs)) {
+			struct tegra_dc_mode *dcmode = &dc->out->modes[0];
+			dcmode->pclk          = specs.modedb->pixclock;
+			dcmode->pclk          = PICOS2KHZ(dcmode->pclk);
+			dcmode->pclk         *= 1000;
+			dcmode->h_ref_to_sync = 1;
+			dcmode->v_ref_to_sync = 1;
+			dcmode->h_sync_width  = specs.modedb->hsync_len;
+			dcmode->v_sync_width  = specs.modedb->vsync_len;
+			dcmode->h_back_porch  = specs.modedb->left_margin;
+			dcmode->v_back_porch  = specs.modedb->upper_margin;
+			dcmode->h_active      = specs.modedb->xres;
+			dcmode->v_active      = specs.modedb->yres;
+			dcmode->h_front_porch = specs.modedb->right_margin;
+			dcmode->v_front_porch = specs.modedb->lower_margin;
+			tegra_dc_set_mode(dc, dcmode);
+			dc->pdata->fb->xres = dcmode->h_active;
+			dc->pdata->fb->yres = dcmode->v_active;
+		}
+	}
+#endif /* CONFIG_FRAMEBUFFER_CONSOLE */
+
 #ifndef CONFIG_TEGRA_ISOMGR
 		/*
 		 * The emc is a shared clock, it will be set based on
